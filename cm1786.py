1511293c2af4a3ff3f911c02e70495839f06b95b
ltulib <magnus.pettersson@ltu.se>
2018-06-28 10:25:52 +0200

    small update

--- cm1786.py
+++ cm1786.py
@@ -6,43 +6,34 @@ import module
 
 config_file = configparser.ConfigParser()
 config_file.read("config.ini")
-
-config = {}
-config['alphabet'] = config_file.get('default', 'alphabet')
-config['alter'] = int(config_file.get('default', 'alter'))
-config['file'] = config_file.get('default', 'file')
-config['num'] = int(config_file.get('default', 'number_of_alphabets'))
-config['outfile'] = config_file.get('default', 'outfile')
+config = dict(config_file.items('default'))
 
 def main():
     global config
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-g', '--generate', default=False, required=False,
-                        help='Generate crypto table', action='store_true')
-    args, unknown = parser.parse_known_args()
-
-    if args.generate == True:
-        print("Generate new chiper alphabet")
-        data = module.crypto_table_dictionary_generate(config['num'], config['alphabet'])
-        module.save_json_to_file(config['file'], data)
-
-        if len(unknown) < 1:
-            exit()
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--alter', help='Alter start position.')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-e', '--encrypt', help='Encrypt', action='store_true')
     group.add_argument('-d', '--decrypt', help='Decrypt', action='store_true')
-    group_input = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-g', '--generate', default=False, help='Generate crypto table', action='store_true')
+    group_input = parser.add_mutually_exclusive_group(required=False)
     group_input.add_argument('-s', '--string', help='input string')
     group_input.add_argument('-i', '--input', help='Open file to encrypt.')
+    # group_input.add_argument('-g', '--generate', default=False, help='Generate crypto table', action='store_true')
     parser.add_argument('-o', '--output', help='Save encrypted text to file.')
     args, unknown = parser.parse_known_args()
 
     print("Chiffre-Machinen 1786")
     print("---------------------\n")
+
+    if args.generate == True:
+        print("Generate new chiper alphabet")
+        data = module.crypto_table_dictionary_generate(int(config['num']), config['alphabet'])
+        module.save_json_to_file(config['file'], data)
+
+        if len(unknown) < 1:
+            exit()
 
     if args.alter:
         config['alter'] = args.alter
