3e6d2fd0fe4075efb201380cddd493721eef0dda
ltulib <magnus.pettersson@ltu.se>
2018-06-29 15:54:55 +0200

    updated argparse

--- cm1786.py
+++ cm1786.py
@@ -8,32 +8,36 @@ config_file = configparser.ConfigParser()
 config_file.read("config.ini")
 config = dict(config_file.items('default'))
 
-def main():
-    global config
+
+class StartGenerate(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        print("Generate new chiper alphabet")
+        data = module.crypto_table_dictionary_generate(int(config['num']), config['alphabet'])
+        module.save_json_to_file(config['file'], data)
+        exit()
+
+if __name__ == '__main__':
+    # add num option
+    # add function to update config file
+    # change how A works, need to change whole msg or eath line
 
     parser = argparse.ArgumentParser()
+    group1 = parser.add_mutually_exclusive_group(required=True)
+    group1.add_argument('-e', '--encrypt', help='Encrypt', action='store_true')
+    group1.add_argument('-d', '--decrypt', help='Decrypt', action='store_true')
+
+    group2 = parser.add_mutually_exclusive_group(required=True)
+    group2.add_argument('-s', '--string', help='input string')
+    group2.add_argument('-i', '--input', help='Open file to encrypt.')
+
     parser.add_argument('-a', '--alter', help='Alter start position.')
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-e', '--encrypt', help='Encrypt', action='store_true')
-    group.add_argument('-d', '--decrypt', help='Decrypt', action='store_true')
-    group.add_argument('-g', '--generate', default=False, help='Generate crypto table', action='store_true')
-    group_input = parser.add_mutually_exclusive_group(required=False)
-    group_input.add_argument('-s', '--string', help='input string')
-    group_input.add_argument('-i', '--input', help='Open file to encrypt.')
-    # group_input.add_argument('-g', '--generate', default=False, help='Generate crypto table', action='store_true')
+    parser.add_argument('-g', "--generate', help='Generates a new chiper alpabeth, can't be used with other flags", action=StartGenerate, nargs=0)
     parser.add_argument('-o', '--output', help='Save encrypted text to file.')
+
     args, unknown = parser.parse_known_args()
 
     print("Chiffre-Machinen 1786")
     print("---------------------\n")
-
-    if args.generate == True:
-        print("Generate new chiper alphabet")
-        data = module.crypto_table_dictionary_generate(int(config['num']), config['alphabet'])
-        module.save_json_to_file(config['file'], data)
-
-        if len(unknown) < 1:
-            exit()
 
     if args.alter:
         config['alter'] = args.alter
@@ -65,7 +69,3 @@ def main():
 
     if config['outfile']:
         module.save_output_to_file(config['outfile'], text)
-
-
-if __name__ == '__main__':
-    main()
